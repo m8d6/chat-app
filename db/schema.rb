c06e4d1e8b53ba252f2edcddebdab31bb8f1ae64
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2025-03-05 12:42:57 +0300

    Revert "1 registration"

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_125258) do
-
+ActiveRecord::Schema[8.0].define(version: 2025_02_28_132238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 

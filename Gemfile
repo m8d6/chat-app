bf165564cc5a061170582575263d7f5fd74566dc
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2025-03-06 17:29:24 +0300

    Reviews completed

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ gem "thruster", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
-
+  gem "dotenv-rails"
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
 
@@ -63,5 +63,3 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
-
-gem "dotenv-rails", groups: [ :development, :test ]
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,29 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal", "openButton", "closeButton"]
+  static targets = ["modal"]
 
   connect() {
-  this.modal = this.modalTarget
-    this.openButton = this.openButtonTarget
-    this.closeButton = this.closeButtonTarget
-
-    this.openButton.addEventListener("click", (event) => this.open(event))
-    this.closeButton.addEventListener("click", () => this.close())
-
-    this.modal.addEventListener("click", (event) => {
-      if (event.target === this.modal) {
-        this.close()
-      }
-    })
+    console.log("Modal controller yüklendi!") 
   }
 
   open(event) {
     event.preventDefault()
-    this.modal.classList.remove("hidden")
+    this.modalTarget.classList.remove("hidden")
   }
 
   close() {
-    this.modal.classList.add("hidden")
+    this.modalTarget.classList.add("hidden")
   }
 }

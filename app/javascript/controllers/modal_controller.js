66045cf646f46bddea6eb6e72dc7539e1c06a618
RumeysaTuysuz <rumeysatysz2002@gmail.com>
2025-03-07 14:02:14 +0300

    updated

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
-  connect() {
-    console.log("Modal controller yüklendi!") 
-  }
-
   open(event) {
     event.preventDefault()
     this.modalTarget.classList.remove("hidden")

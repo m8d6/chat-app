335d3a1c976c3f80647834217838c806682fbfbb
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-03-14 12:53:42 +0300

    assertion_helper

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module ChatApp
     # config.eager_load_paths << Rails.root.join("extras")
     
     # Subdirectories in locales folder
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+  
   end
 end

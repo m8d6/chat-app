e2223c72d8a572cb63c5bab04312b3e91d66fe17
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-03-14 11:15:45 +0300

    create AssertionsHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,17 +3,14 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+
+  
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+
   fixtures :all
 
- 
-  def assert_error_on(record, attribute, error_type)
-    assert_not record.valid?
-    assert_not_empty record.errors.where(attribute, error_type)
-  end
+  include AssertionsHelper
 
   
 end

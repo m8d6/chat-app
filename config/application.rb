a7d7f8565b29d3e030c064f171b67e1d6c63f1ac
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-03-14 11:34:36 +0300

    missing i18n message added

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module ChatApp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -24,5 +24,8 @@ module ChatApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # Subdirectories in locales folder
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end

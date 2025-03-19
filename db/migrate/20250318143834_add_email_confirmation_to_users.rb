8fa4e44f097b6c491136078995e175d354f71043
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-03-19 14:16:40 +0300

    migration error fix

--- db/migrate/20250318143834_add_email_confirmation_to_users.rb
+++ db/migrate/20250318143834_add_email_confirmation_to_users.rb
@@ -1,8 +1,9 @@
 class AddEmailConfirmationToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :confirmation_token, :string
-    add_index :users, :confirmation_token
-    add_column :users, :email_confirmed, :boolean
-    add_column :users, :email_verified_at, :datetime
+    add_column :users, :confirmation_token, :string unless column_exists?(:users, :confirmation_token)
+    add_column :users, :email_confirmed, :boolean, default: false unless column_exists?(:users, :email_confirmed)
+    add_column :users, :email_verified_at, :datetime unless column_exists?(:users, :email_verified_at)
+    
+    add_index :users, :confirmation_token, unique: true unless index_exists?(:users, :confirmation_token)
   end
 end

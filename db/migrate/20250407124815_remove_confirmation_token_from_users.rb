65b51f22cc27fcf804957210b8e72313cde825b0
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-04-14 16:10:50 +0300

    use fixture copies in user tests

--- db/migrate/20250407124815_remove_confirmation_token_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveConfirmationTokenFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_index :users, :confirmation_token if index_exists?(:users, :confirmation_token)
-    remove_column :users, :confirmation_token, :string if column_exists?(:users, :confirmation_token)
-  end
-end

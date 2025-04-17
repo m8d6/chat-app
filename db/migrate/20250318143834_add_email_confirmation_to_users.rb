35dcf0f9e7e62ac7f33eb1e1c73992477ae7f5e0
Mehmet Ali Çavuşlu <90102689+croftwell@users.noreply.github.com>
2025-04-17 14:18:37 +0300

    Simplify AddEmailConfirmationToUsers migration

--- db/migrate/20250318143834_add_email_confirmation_to_users.rb
+++ db/migrate/20250318143834_add_email_confirmation_to_users.rb
@@ -1,8 +1,8 @@
 class AddEmailConfirmationToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :email_confirmed, :boolean, default: false unless column_exists?(:users, :email_confirmed)
-    add_column :users, :email_verified_at, :datetime unless column_exists?(:users, :email_verified_at)
+    add_column :users, :email_confirmed, :boolean, default: false
+    add_column :users, :email_verified_at, :datetime
 
-    add_index :users, :confirmation_token, unique: true unless index_exists?(:users, :confirmation_token)
+    add_index :users, :confirmation_token, unique: true
   end
 end
